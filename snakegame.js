e107d07f732fa1b7af94db0eed4924059b07b4db
Anuja Saharkar <anujasaharkar41@gmail.com>
2025-11-27 14:28:55 +0530

    js added

--- snakegame.js
+++ snakegame.js
@@ -5,20 +5,23 @@ var btnrestart=document.querySelector('.btn-restart');
 var modal1=document.querySelector('.modal1')
 var restartgame=document.querySelector('.restartgame');
 var startgame=document.querySelector('.startgame');
-var time=document.querySelector('#time');
+var timeele=document.querySelector('#time');
 var highscore=document.querySelector('#highscore');
 const blockheight=30;
 const blockwidth=30;
+var gamespeed=300;
 
 const cols=Math.floor(board.clientWidth/blockwidth);
 const rows=Math.floor(board.clientHeight/blockheight);
 
 var blocks=[];
 var score=0;
-var hgh=localStorage.getItem("highscore") || 0;
+var time='00:00';
+var hgh=Number(localStorage.getItem("highscore")) || 0;
 var snake=[{x:1,y:3},{x:1,y:4},{x:1,y:5}];
 var direction='right';
 var intervalid=null;
+var timerid=null;
 var food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
 
 highscore.innerText=hgh;
@@ -82,7 +85,8 @@ function drawsnake(){
         if(score>hgh)
         {
             hgh=score;
-            localStorage.setItem("highscore",hgh.toString);
+            localStorage.setItem("highscore",hgh.toString());
+            highscore.innerText=hgh;
         }
         blocks[`${food.x},${food.y}`].classList.add('food');
         snake.unshift(head);
@@ -125,26 +129,71 @@ addEventListener('keydown',function(elem){
 })
 
 btnstart.addEventListener('click',function(){
-    intervalid=setInterval(function(){
-        drawsnake();
-    },300);
+     clearInterval(intervalid);
+    clearInterval(timerid);
+    intervalid=setInterval(drawsnake,gamespeed);
     modal1.style.display='none';
+    timerid=setInterval(function(){
+        let[min,sec]=time.split(':').map(Number);
+
+        if(sec===59)
+        {
+            min+=1;
+            sec=0;
+        }
+        else
+        {
+            sec+=1;
+        }
+
+        time=`${min}:${sec}`;
+        timeele.innerText=time;
+    },1000)
 })
 
 function restart(){
+    clearInterval(intervalid);   // <<-- IMPORTANT
+    clearInterval(timerid);
     blocks[`${food.x},${food.y}`].classList.remove('food');
     snake.forEach(function(elem){
       blocks[`${elem.x},${elem.y}`].classList.remove('fill');
     });
     score=0;
-    time='00:00';
+    time='00:00'
+    timeele.innerText=time;
     modal1.style.display='none';
     direction='down';
     snake=[{x:1,y:3},{x:1,y:4},{x:1,y:5}];
     food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
-    intervalid=setInterval(function(){
-        drawsnake();
-    },300);
+    timerid=setInterval(function(){
+        let[min,sec]=time.split(':').map(Number);
+
+        if(sec===59)
+        {
+            min+=1;
+            sec=0;
+        }
+        else
+        {
+            sec+=1;
+        }
+
+        time=`${min}:${sec}`;
+        timeele.innerText=time;
+    },1000)
+    intervalid=setInterval(drawsnake,gamespeed);
+
 }
 
+
 btnrestart.addEventListener('click',restart);
+
+document.querySelectorAll('.btn-diff').forEach(function(btn){
+    btn.addEventListener('click',function(){
+        gamespeed=Number(btn.dataset.speed);
+
+        document.querySelectorAll('.btn-diff').forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+
+    })
+})
