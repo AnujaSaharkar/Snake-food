7094dfa280c7434e80540af26f42a642a20f3485
Anuja Saharkar <anujasaharkar41@gmail.com>
2025-11-27 17:08:23 +0530

    Keep only required Snake game files

--- jsproj3.js
+++
@@ -1,8 +0,0 @@
-var img=document.querySelector('img');
-var love=document.querySelector('i');
-
-img.addEventListener('dblclick',function(){
-    love.style.opacity=1;
-    love.style.color='crimson';
-    love.style.transform=`translate(-50%,-50%) scale(0.98)`;
-})
--- jsproj6.js
+++
@@ -1,8 +0,0 @@
-var main=document.querySelector('main');
-var crsr=document.querySelector('#cursor');
-
-main.addEventListener('mousemove',function(move){
-    crsr.style.left=move.x+'px';
-    crsr.style.top=move.y+'px';
-
-})
